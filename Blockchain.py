83b2678c9853e852f5358afe68b1d35c65a894f6
mutaeb <mutaeb1@outlook.sa>
2023-12-08 09:28:19 +0300

    Part D. I have implemented a floating-point difficulty level and a simple (Multiplicative) cipher for the amount.

--- Blockchain.py
+++ Blockchain.py
@@ -1,38 +1,40 @@
 # import all the necessary libraries and modules
 
-import sys
 import hashlib
 import json
+import sys
 from time import time
-from uuid import uuid4
-from flask import Flask, jsonify, request
-import requests
 from urllib.parse import urlparse
+from uuid import uuid4
+
+import requests
+from flask import Flask, jsonify, request
 
 
+def encrypt(message, multiple):
+    return "".join(chr((ord(char) * multiple) % 256) for char in str(message))
+
+
+def decrypt(message, multiple):
+    inverse = 0
+    for i in range(256):
+        if (multiple * i) % 256 == 1:
+            inverse = i
+            break
+    return "".join(chr((ord(char) * inverse) % 256) for char in str(message))
+
+
+# noinspection PyMethodMayBeStatic
 class Blockchain(object):
+    # The __init__() function is the constructor for the class. Here, you
+    # store the entire blockchain as a list. Because every blockchain has a
+    # genesis block, you need to initialize the genesis block with the hash
+    # of the previous block, and in this example, we simply used a fixed
+    # string called “genesis_block” to obtain its hash. Once the hash of the
+    # previous block is found, we need to find the nonce for the block using
+    # the method named proof_of_work() (which we will define in the
+    # next section).
 
-    difficulty_target = "0000"
-
-    # The hash_block() method encodes a block into array of bytes and
-    # then hashes it; you need to ensure that the dictionary is sorted, or
-    # you’ll have inconsistent hashes later on
-
-    def hash_block(self, block):
-
-        block_encoded = json.dumps(block, sort_keys=True).encode()
-
-        return hashlib.sha256(block_encoded).hexdigest()
-    
-    #The __init__() function is the constructor for the class. Here, you
-    #store the entire blockchain as a list. Because every blockchain has a
-    #genesis block, you need to initialize the genesis block with the hash
-    #of the previous block, and in this example, we simply used a fixed
-    #string called “genesis_block” to obtain its hash. Once the hash of the
-    #previous block is found, we need to find the nonce for the block using
-    #the method named proof_of_work() (which we will define in the
-    #next section).
-    
     def __init__(self):
         self.nodes = set()
 
@@ -42,13 +44,22 @@ class Blockchain(object):
         # temporarily stores the transactions for the current block
         self.current_transactions = []
 
+        self.difficulty = 1
+
+        self.target_time = 5
+
+        self.multiple = 7
+
         # create the genesis block with a specific fixed hash
         # of previous block genesis block starts with index 0
-        genesis_hash = self.hash_block("genesis_block")
-        self.append_block(
-        hash_of_previous_block = genesis_hash,
-        nonce = self.proof_of_work(0, genesis_hash, [])
-        )
+        hash, nonce, time_spent = self.proof_of_work(0, 0, [])
+        self.append_block(hash, nonce, time_spent, 0)
+
+    def adjust_difficulty(self):
+        mean_time = sum(block['time_spent'] for block in self.chain[-4:]) / min(4, len(self.chain))
+        self.difficulty *= (mean_time == self.target_time and 1 or mean_time > self.target_time and 0.9 or 1.2)
+        if self.difficulty < 1:
+            self.difficulty = 1
 
     def add_node(self, address):
         parsed_url = urlparse(address)
@@ -57,30 +68,29 @@ class Blockchain(object):
 
     # determine if a given blockchain is valid
     def valid_chain(self, chain):
-        last_block = chain[0] # the genesis block
-        current_index = 1 # starts with the second block
+        last_block = chain[0]  # the genesis block
+        current_index = 1  # starts with the second block
         while current_index < len(chain):
             block = chain[current_index]
-            if block['hash_of_previous_block'] != self.hash_block(last_block):
+            if block['previous_hash'] != last_block['hash']:
                 return False
-        
-            # check for valid nonce
-            if not self.valid_proof(current_index, 
-                block['hash_of_previous_block'],
-                block['transactions'],
-                block['nonce']):
 
+            # check for valid nonce
+            if block['hash'] != self.calculate_hash(current_index,
+                                                    block['previous_hash'],
+                                                    block['transactions'],
+                                                    block['nonce']):
                 return False
-            
+
             # move on to the next block on the chain
             last_block = block
             current_index += 1
 
-            # the chain is valid
-            return True
-        
+        # the chain is valid
+        return True
+
     def update_blockchain(self):
-        # get the nodes around us that has been registered
+        # get the surrounding nodes that has been registered
         neighbours = self.nodes
         new_chain = None
         # for simplicity, look for chains longer than ours
@@ -103,136 +113,139 @@ class Blockchain(object):
         if new_chain:
             self.chain = new_chain
             return True
-        
+
         return False
 
-    #The proof_of_work() method (detailed next) will return a nonce that will result in a
-    #hash that matches the difficulty target when the content of the current block is hashed.
-    #For simplicity, we are fixing the difficulty_target to a hash result that starts with four zeros (“0000”).
+    # The proof_of_work() method (detailed next) will return a nonce that will result in a
+    # hash that matches the difficulty target when the content of the current block is hashed.
+    # For simplicity, we are fixing the difficulty_target to a hash result that starts with four zeros (“0000”).
 
-    #The proof_of_work() function first starts with zero for the nonce and check if the
-    #nonce together with the content of the block produces a hash that matches the difficulty
-    #target. If not, it increments the nonce by one and then try again until it finds the correct nonce.
+    # The proof_of_work() function first starts with zero for the nonce and check if the
+    # nonce together with the content of the block produces a hash that matches the difficulty
+    # target. If not, it increments the nonce by one and then try again until it finds the correct nonce.
 
     # use PoW to find the nonce for the current block
-    def proof_of_work(self, index, hash_of_previous_block, transactions):
+    def proof_of_work(self, index, previous_hash, transactions):
         # try with nonce = 0
         nonce = 0
+        hash = ''
+        start_time = time()
         # try hashing the nonce together with the hash of the
         # previous block until it is valid
-        while self.valid_proof(index, hash_of_previous_block, transactions, nonce) is False:
+        while self.meets_difficulty(hash) is False:
             nonce += 1
-        return nonce
-    
-    #The next method, valid_proof(), hashes the content of a block and check to see if
-    #the block’s hash meets the difficulty target:
+            hash = self.calculate_hash(index, previous_hash, transactions, nonce)
 
-    def valid_proof(self, index, hash_of_previous_block, transactions, nonce):
+        return hash, nonce, time() - start_time
 
-        # create a string containing the hash of the previous block and the block content, including the nonce
-        content = f'{index}{hash_of_previous_block}{transactions}{nonce}'.encode()
+    # The next method, valid_proof(), hashes the content of a block and check to see if
+    # the block’s hash meets the difficulty target:
 
-        # hash using sha256
-        content_hash = hashlib.sha256(content).hexdigest()
-
+    def meets_difficulty(self, hash):
         # check if the hash meets the difficulty target
-        return content_hash[:len(self.difficulty_target)] == self.difficulty_target
-    
+        return hash.startswith('0' * int(self.difficulty))
+
+    def calculate_hash(self, index, previous_block, transactions, nonce):
+        return hashlib.sha256(f'{index}{previous_block}{transactions}{nonce}'.encode()).hexdigest()
 
     # creates a new block and adds it to the blockchain
-    def append_block(self, nonce, hash_of_previous_block):
+    def append_block(self, hash, nonce, time_spent, previous_hash=None):
 
         block = {
+            'hash': hash,
             'index': len(self.chain),
             'timestamp': time(),
             'transactions': self.current_transactions,
             'nonce': nonce,
-            'hash_of_previous_block': hash_of_previous_block}
-        
+            'time_spent': time_spent,
+            'previous_hash': previous_hash
+        }
+
         # reset the current list of transactions
         self.current_transactions = []
 
         # add the new block to the blockchain
         self.chain.append(block)
 
+        self.adjust_difficulty()
+
         return block
-    
+
     def add_transaction(self, sender, recipient, amount):
 
         self.current_transactions.append({
-            'amount': amount,
+            'amount': encrypt(amount, self.multiple),
             'recipient': recipient,
             'sender': sender,
         })
-        
+
         return self.last_block['index'] + 1
-    
+
     @property
     def last_block(self):
         # returns the last block in the blockchain
         return self.chain[-1]
-    
+
+    @property
+    def last_hash(self):
+        # returns the last block in the blockchain
+        return self.last_block['hash']
 
 
 app = Flask(__name__)
 # generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', "") ###################
+node_identifier = str(uuid4()).replace('-', "")
 # instantiate the Blockchain
 blockchain = Blockchain()
 
+
 # return the entire blockchain
 @app.route('/blockchain', methods=['GET'])
-
 def full_chain():
-
     response = {
-    'chain': blockchain.chain,
-    'length': len(blockchain.chain),
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
     }
 
     return jsonify(response), 200
 
+
 @app.route('/mine', methods=['GET'])
-
 def mine_block():
-
-    blockchain.add_transaction(
-    sender="0",
-    recipient=node_identifier,
-    amount=1,)
+    blockchain.add_transaction(sender="0", recipient=node_identifier, amount=1)
 
     # obtain the hash of last block in the blockchain
-    last_block_hash = blockchain.hash_block(blockchain.last_block)
+    last_block_hash = blockchain.last_hash
 
     # using PoW, get the nonce for the new block to be added to the blockchain
     index = len(blockchain.chain)
-    nonce = blockchain.proof_of_work(index, last_block_hash, blockchain.current_transactions)
+    hash, nonce, time_spent = blockchain.proof_of_work(index, last_block_hash, blockchain.current_transactions)
 
     # add the new block to the blockchain using the last block
     # hash and the current nonce
-    block = blockchain.append_block(nonce, last_block_hash)
+    block = blockchain.append_block(hash, nonce, time_spent, last_block_hash)
 
     response = {
-    'message': "New Block Mined",
-    'index': block['index'],
-    'hash_of_previous_block':
-    block['hash_of_previous_block'],
-    'nonce': block['nonce'],
-    'transactions': block['transactions'],
+        'message': "New Block Mined",
+        'hash': hash,
+        'index': index,
+        'previous_hash': last_block_hash,
+        'nonce': nonce,
+        'time_spent': time_spent,
+        'transactions': block['transactions'],
     }
     return jsonify(response), 200
 
+
 @app.route('/transactions/new', methods=['POST'])
-
 def new_transaction():
-
     # get the value passed in from the client
     values = request.get_json()
 
     # check that the required fields are in the POST'ed data
     required_fields = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required_fields):
-        return ('Missing fields', 400)
+        return 'Missing fields', 400
 
     # create a new transaction
     index = blockchain.add_transaction(
@@ -243,40 +256,43 @@ def new_transaction():
 
     response = {'message': f'Transaction will be added to Block {index}'}
 
-    return (jsonify(response), 201)
+    return jsonify(response), 201
+
 
 @app.route('/nodes/add_nodes', methods=['POST'])
 def add_nodes():
     # get the nodes passed in from the client
-    values = request.get_json()
+    values = json.loads(request.data, strict=False)
     nodes = values.get('nodes')
     if nodes is None:
         return "Error: Missing node(s) info", 400
-    
+
     for node in nodes:
         blockchain.add_node(node)
         response = {
             'message': 'New nodes added',
             'nodes': list(blockchain.nodes),
-            }
-    return jsonify(response), 201
+        }
+        return jsonify(response), 201
+
 
 @app.route('/nodes/sync', methods=['GET'])
 def sync():
     updated = blockchain.update_blockchain()
     if updated:
         response = {
-        'message':
-        'The blockchain has been updated to the latest',
-        'blockchain': blockchain.chain
+            'message':
+                'The blockchain has been updated to the latest',
+            'blockchain': blockchain.chain
         }
     else:
         response = {
-        'message': 'Our blockchain is the latest',
-        'blockchain': blockchain.chain
+            'message': 'Our blockchain is the latest',
+            'blockchain': blockchain.chain
         }
 
     return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(sys.argv[1]))
